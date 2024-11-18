1083dfde28cd6fafda217d8afbbf5ea1ce41c595
andrei <angpeu@gmail.com>
2024-11-18 03:20:32 +0200

    initial

--- tests/api-login.spec.ts
+++ tests/api-login.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test'
 import { LoginDto } from './dto/LoginDto'
 import { StatusCodes } from 'http-status-codes'
+import { JWTchecker } from '../src/JWTchecker'
 
 const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
@@ -26,6 +27,44 @@ test.describe('Tests for POST login/student', () => {
       const responseBody = await response.text()
       console.log('Response body: ', responseBody)
       expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+    })
+  })
+
+  test.describe('Tests from Homework 11', () => {
+    let jwtchecker: JWTchecker
+    test.beforeEach(() => {
+      jwtchecker = new JWTchecker()
+    })
+
+    test('login with valid data should return valid JWT in response', async ({ request }) => {
+      const requestBody = LoginDto.createLoginWithCorrectData()
+      const response = await request.post(`${serviceURL}${loginPath}`, {
+        data: requestBody,
+      })
+      expect(response.status()).toBe(StatusCodes.OK)
+      const responseBody = await response.text()
+      console.log('Response body: ', responseBody)
+      expect(jwtchecker.isValidJWT(responseBody)).toBe(true)
+    })
+
+    test('should return no JWT in response if unauthorized', async ({ request }) => {
+      const requestBody = LoginDto.createLoginWithIncorrectData()
+      const response = await request.post(`${serviceURL}${loginPath}`, {
+        data: requestBody,
+      })
+      expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+      const responseBody = await response.text()
+      expect(jwtchecker.isValidJWT(responseBody)).toBe(false)
+    })
+
+    test('invalid http request method, should return status code 405', async ({ request }) => {
+      const requestBody = LoginDto.createLoginWithCorrectData()
+      const response = await request.put(`${serviceURL}${loginPath}`, {
+        data: requestBody,
+      })
+      expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+      const responseBody = await response.text()
+      expect(jwtchecker.isValidJWT(responseBody)).toBe(false)
     })
   })
 })
